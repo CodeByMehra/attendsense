796b20f87f38362eff40ab4b8ce73de2bb83ab69
CodeByMehra <kpvishalmehra@gmail.com>
2026-07-09 09:25:26 +0530

    added reuquired files

---
+++ src/screens/home_screen.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def main():
+    st.header("Hey this is Home Screen")
+main()
---
+++ src/screens/techer_screen.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def main():
+    st.header("Hey this is teacher Screen")
+main()
