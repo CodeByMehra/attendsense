2e4606386259423e9b7e5f052c14786ed7f55abb
CodeByMehra <kpvishalmehra@gmail.com>
2026-07-10 13:38:40 +0530

    working on teacher screen

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from src.components.header import header_home
-from ui.base_layout import style_base_layout, style_background_home
+from src.ui.base_layout import style_base_layout, style_background_home
 from src.components.footer import footer_home
 
 def home_screen():
--- src/screens/techer_screen.py
+++ src/screens/techer_screen.py
@@ -1,4 +1,97 @@
 import streamlit as st
 
+from src.components.header import header_dashboard
+from src.components.footer import footer_dashboard
+from src.ui.base_layout import (
+    style_background_dashboard,
+    style_base_layout,
+)
+
+
 def teacher_screen():
-    st.header("Hey this is teacher Screen")
+    style_background_dashboard()
+    style_base_layout()
+
+    
+    teacher_screen_login()
+    
+    
+def teacher_screen_login():
+    c1, c2 = st.columns(
+        2,
+        vertical_alignment="center",
+        gap="xxlarge",
+    )
+
+    with c1:
+        header_dashboard()
+
+    with c2:
+        st.button(
+            "Go Bak To Home",
+            type="secondary",
+            key="loginbackbtn",
+            shortcut="control+backspace",
+        )
+
+    st.header("Login using password", text_alignment="center")
+    st.space()
+    st.space()
+
+    teacher_username = st.text_input(
+        "Enter Username",
+        placeholder="eg: professor1969",
+    )
+
+    teacher_password = st.text_input(
+        "Enter Your Password",
+        type="password",
+        placeholder="Enter Password",
+    )
+
+    st.divider()
+
+    btnc1, btnc2 = st.columns(2)
+
+    with btnc1:
+        st.button(
+            "Login",
+            icon=":material/passkey:",
+            shortcut="control+Enter",
+            width="stretch",
+        )
+    
+    with btnc2:
+        st.button(
+            "Register Instead",
+            type="primary",
+            icon=":material/passkey:",
+            width="stretch",
+            
+        )
+
+    footer_dashboard()
+        
+    
+
+def teacher_screen_register():
+    c1, c2 = st.columns(
+        2,
+        vertical_alignment="center",
+        gap="xxlarge",
+    )
+
+    with c1:
+        header_dashboard()
+
+    with c2:
+        st.button(
+            "Go Bak To Home",
+            type="secondary",
+            key="loginbackbtn",
+            shortcut="comtrol+backspace",
+        )
+
+    st.header("Register your teacher profile")
+
+    footer_dashboard()
