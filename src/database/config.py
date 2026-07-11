a29bdabb59e3ba35c9c4dbd13d8ecab1f0f3baa2
CodeByMehra <kpvishalmehra@gmail.com>
2026-07-11 09:52:00 +0530

    fixed bug from config.py file

--- src/database/config.py
+++ src/database/config.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-from supabase import create_client , client
+from supabase import create_client , Client
 
-supabase: Client = create_client(
+supabase:Client = create_client(
     st.secrets["SUPABASE_URL"],
     st.secrets["SUPABASE_KEY"]
 )
