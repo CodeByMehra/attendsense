aa8bd45cb0dd97edf1dae5063ae16bfd742335df
CodeByMehra <kpvishalmehra@gmail.com>
2026-07-11 09:41:17 +0530

    creating functions for teacher table

--- src/database/db.py
+++ src/database/db.py
@@ -1,3 +1,18 @@
 from src.database.config import supabase
 
 import bcrypt
+
+# function to check if username already taken
+def check_teacher_exists(username):
+    response = supabase.table("teachers").select("username").eq("username", username).execute()
+    return len(response.data) > 0  # this line returns 1(true) on if username matches
+
+# function to create teacher
+def create_teacher(username, password, name):
+    data = {
+        "username" : username,
+        "password" : hash_pass(password), #hashing using bcrypt
+        "name" : name
+    }
+    response = supabase.table("teachers").insert(data).execute()
+    return response.data
