61c23a6f641f13d10e9ec85d96ae8d5e0d3e3e20
CodeByMehra <kpvishalmehra@gmail.com>
2026-07-13 09:35:33 +0530

    function to get face embeddings:

--- src/pipelines/face_pipeline.py
+++ src/pipelines/face_pipeline.py
@@ -1,1 +1,39 @@
 # Face Image -> Face Detector (dlib) -> Shape Predictor(sp) and detect landmarks-> ResNet (facerec) 128D -> SVM Classifier 
+
+import dlib
+import numpy as np 
+import face_recognition_models
+from sklearn.svm import SVC
+import streamlit as st
+
+from src.database.db import get_all_students
+
+
+@st.cache_resource 
+def load_dlib_models(): 
+    detector = dlib.get_frontal_face_detector()
+    
+    sp = dlib.shape_predictor(
+        face_recognition_models.pose_predictor_model_location()
+    )
+    
+    facerec = dlib.face_recognition_model_v1(
+        face_recognition_models.face_recognition_model_location()
+    )
+    
+    return detector,sp,facerec
+
+
+# function to get face embeddings:
+def get_face_embeddings(image_np):
+    detector, sp, facerec = load_dlib_models()
+    faces = detector(image_np, 1)
+    
+    encodings = []
+    
+    for face in faces:
+        shape = sp(image_np, face)
+        face_descriptor = facerec.compute_face__descriptor(image_np, shape, 1)
+        
+        encodings.append(np.array(face_descriptor))
+    return encodings
